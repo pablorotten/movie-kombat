530934456c0e0e16882a3f167d8102b11fb6ea96
PRO <pro@n-side.com>
2025-04-27 01:54:15 +0200

    Dynamically add movie name to card

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import movieKombatLogo from "./assets/movie-kombat-logo.svg";
 import "./App.css";
 
-function addMovie() {
-  console.log("Add movie button clicked with parameter:");
-}
+// function addMovie(movieName: string) {
+//   console.log("Add movie button clicked with name:", movieName);
+// }
 
 function App() {
+  const [movie, setSelectedTopic] = useState();
+
+  function handleAddMovie(movieData) {
+    setSelectedTopic(movieData);
+  }
+
+  let movieCard = (
+    <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e dark:bg-gray-800 dark:border-gray-700">
+      <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+          {movie}
+        </h3>
+      </blockquote>
+      <figcaption className="flex items-center justify-center">
+        <img
+          className=""
+          src="img/poster-placeholder.png"
+          alt="profile picture"
+        />
+      </figcaption>
+    </figure>
+  );
+
   return (
     <>
       <header className="flex items-center p-4 bg-gray-800 text-white">
@@ -41,12 +65,21 @@ function App() {
           <button
             type="button"
             id="addMovieButton"
-            onClick={addMovie}
+            onClick={() => handleAddMovie("Added movie")}
             className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Add
             <span className="sr-only">Search</span>
           </button>
+        </div>
+        <div id="movieInfo"></div>
+      </div>
+      <div className="container mx-auto">
+        <div
+          id="movieGrid"
+          className="grid mb-8 border border-gray-200 rounded-lg shadow-xs dark:border-gray-700 md:mb-12 md:grid-cols-6 bg-white dark:bg-gray-800"
+        >
+          {movieCard}
         </div>
       </div>
     </>
