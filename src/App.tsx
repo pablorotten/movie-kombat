9b720a77a8a5e5ba94ad233ba39f24375ffdf498
PRO <pro@n-side.com>
2025-04-27 01:58:55 +0200

    Fix some errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,14 @@ import { useState } from "react";
 import movieKombatLogo from "./assets/movie-kombat-logo.svg";
 import "./App.css";
 
-// function addMovie(movieName: string) {
-//   console.log("Add movie button clicked with name:", movieName);
-// }
-
 function App() {
-  const [movie, setSelectedTopic] = useState();
+  const [movie, setSelectedTopic] = useState<string | undefined>();
 
-  function handleAddMovie(movieData) {
+  function handleAddMovie(movieData: string) {
     setSelectedTopic(movieData);
   }
 
-  let movieCard = (
+  const movieCard = (
     <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e dark:bg-gray-800 dark:border-gray-700">
       <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
         <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
