824ea506ba54444a6bcc361cac3142c898e09e7a
Pablo <pablorottendev@gmail.com>
2025-05-25 23:44:05 +0200

    1 switch to typescript vite react tailwind (#7)
    * #1 - Installed vite with react
    
    * #1 - Add Tailwind
    
    * #1 Create home page: Step 1
    
    * Dynamically add movie name to card
    
    * Fix some errors
    
    * Adding movies with React
    
    ---------
    
    Co-authored-by: PRO <pro@n-side.com>

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import movieKombatLogo from "./assets/movie-kombat-logo.svg";
+import "./App.css";
+import MovieCard from "./components/MovieCard";
+import movieImg from "./assets/movie.png";
+
+
+function App() {
+  
+  const [movieList, setMovieList] = useState<string[]>([]);
+
+  function handleAddMovie(movieTitle: string) {
+    const newList = [...movieList, movieTitle];
+    setMovieList(newList);
+  }
+
+  return (
+    <>
+      <header className="flex items-center p-4 bg-gray-800 text-white">
+        <img
+          src={movieKombatLogo}
+          alt="Movie Kombat Logo"
+          className="h-8 w-8 mr-2"
+        />
+        <h1 className="text-2xl font-bold">Movie Kombat</h1>
+      </header>
+
+      <div className="max-w-3xl mx-auto text-center mt-16">
+        <h1 className="text-4xl font-bold text-gray-900 leading-tight mb-2 pb-4 relative">
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-pink-500">
+            Add your movies
+          </span>
+          <span className="absolute bottom-0 left-0 w-full h-1 bg-gradient-to-r from-purple-500 to-pink-500"></span>
+        </h1>
+        <p className="text-lg mb-8">Search and add 8 🎬 movies</p>
+      </div>
+
+      <div className="max-w-xl mx-auto">
+        <div className="flex items-center max-w-sm mx-auto">
+          <div className="relative w-full">
+            <input
+              type="text"
+              id="movieInput"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search movie..."
+              required
+            />
+          </div>
+          <button
+            type="button"
+            id="addMovieButton"
+            onClick={() => handleAddMovie("Added movie")}
+            className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Add
+            <span className="sr-only">Search</span>
+          </button>
+        </div>
+        <div id="movieInfo"></div>
+      </div>
+      <div className="container mx-auto">
+        <div
+          id="movieGrid"
+          className="grid mb-8 border border-gray-200 rounded-lg shadow-xs dark:border-gray-700 md:mb-12 md:grid-cols-6 bg-white dark:bg-gray-800"
+        >
+          {movieList.map((movie, index) => ( 
+            <MovieCard key={index} title={movie} poster={movieImg} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
