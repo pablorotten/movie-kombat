b71917ccf47e91a205ef3b71c6ab32a6807b4fb5
pablorotten <pablorotten@gmail.com>
2025-08-26 00:25:43 +0200

    cleanup

--- src/utils/placeholderUtils.ts
+++ src/utils/placeholderUtils.ts
@@ -1,8 +1,6 @@
-// This uses Vite's `import.meta.glob` feature to import all .jpeg files
-// from the movie-placeholder folder as URLs.
 const placeholderModules = import.meta.glob('../assets/movie-placeholder/*.jpeg', { eager: true, as: 'url' });
 
-// This converts the object of modules into a simple array of image paths.
+// Converts the object of modules into a simple array of image paths.
 const placeholderImages = Object.values(placeholderModules);
 
 export const getFirstPlaceholder = (): string => {
