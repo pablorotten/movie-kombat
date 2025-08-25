f7094b1a376b7ce6d39d7651911412ee893a61d3
pablorotten <pablorotten@gmail.com>
2025-08-26 00:31:25 +0200

    Fix 'React' is declared but its value is never read

--- src/components/PosterImage.tsx
+++ src/components/PosterImage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { getFirstPlaceholder } from '../utils/placeholderUtils';
 
 interface PosterImageProps {
