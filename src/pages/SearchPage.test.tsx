ec912f15ac146a661f375b209d45110d2b91d8f3
Pablo <pablorottendev@gmail.com>
2026-05-03 00:06:11 +0200

    refactor: remove unused import of act in SearchPage tests

--- src/pages/SearchPage.test.tsx
+++ src/pages/SearchPage.test.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { act, fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { beforeEach, afterEach, describe, expect, it, vi } from 'vitest'
 import { MemoryRouter } from 'react-router-dom'
 import SearchPage from './SearchPage'
